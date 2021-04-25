25a7de6c52345fe64107e14d1c938ecfbac188b1
jeffssant <santana.jeff@gmail.com>
2021-04-25 14:46:37 -0300

    separando as importaçoes dos modulo primeNg

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Pipes';
+
+  nombre: string = "Jeff";
+
+  mostrarNombre(){
+    console.log(this.nombre);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,17 +2,17 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { PrimeNgModule } from './prime-ng/prime-ng.module';
 
 //PrimeNg
-import {ButtonModule} from 'primeng/button';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule,
-    ButtonModule
+    BrowserModule,  
+    PrimeNgModule
   ],
   providers: [],
   bootstrap: [AppComponent]
