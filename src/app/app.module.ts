1a014331801eebac02e779ac2ec62b7a964d154b
jeffssant <santana.jeff@gmail.com>
2021-04-26 20:35:20 -0300

    mudar idioma global para PT

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,16 @@
-import { NgModule } from '@angular/core';
+import { LOCALE_ID, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { AppRouterModule } from './app-router.module';
 import { SharedModule } from './shared/shared.module';
 import { VentasModule } from './ventas/ventas.module';
+
+// Cambiar el locale de la app
+import localePt from '@angular/common/locales/pt';
+import {registerLocaleData} from '@angular/common';
+
+registerLocaleData(localePt);
 
 
 //PrimeNg
@@ -17,9 +23,13 @@ import { VentasModule } from './ventas/ventas.module';
     BrowserModule,      
     SharedModule,
     AppRouterModule, 
-    VentasModule
+    VentasModule,
   ],
-  providers: [],
+  providers: [
+    {
+      provide: LOCALE_ID, useValue: 'pt'
+    }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
